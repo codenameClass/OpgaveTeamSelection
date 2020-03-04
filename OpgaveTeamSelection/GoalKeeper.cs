e355f9ed65163cc59403c48124a41172853d7d79
Sven Coppens <sven.coppens@student.hogent.be>
2020-03-04 11:45:16 +0100

    Speler inheritance push1

--- OpgaveTeamSelection/GoalKeeper.cs
+++ OpgaveTeamSelection/GoalKeeper.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.Linq;
 namespace OpgaveTeamSelection
 {
     class GoalKeeper : Speler
     {
+        public GoalKeeper(string naam, int rugNummer, int rating, int caps, IEnumerable<GoalKeeperPosities> posities) : base(naam, rugNummer, rating, caps)
+        {
+            MogenlijkePosities = posities.ToList();
+        }
+        List<GoalKeeperPosities> MogenlijkePosities { get; set; }
     }
 }
