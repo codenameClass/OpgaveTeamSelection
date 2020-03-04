4805fa88f768f496c59ecc0f1ef1b1438ed36ed1
Soufyan Zyadi <soufyan.zyadi@gmail.com>
2020-03-04 12:30:33 +0100

    bestandlezen

--- OpgaveTeamSelection/FileProcessor.cs
+++ OpgaveTeamSelection/FileProcessor.cs
@@ -6,5 +6,32 @@ namespace OpgaveTeamSelection
 {
     class FileProcessor
     {
+        //bestandlezen
+        //per team playerfactory
+
+        public string source { get; set; }
+
+        public static void ReadFile()
+        {
+            bool result = false;
+            try
+            {
+                // een bestand inlezen. 
+                using (StreamReader sr = new StreamReader(this.fileName))
+                {
+                    // Lees tot het einde van het bestand
+                    this.text = sr.ReadToEnd();
+                    this.melding = String.Format("bestandlezen: ", this.source);
+                    result = true;
+                }
+            }
+            catch (Exception e)
+            {
+                // foutieve melding             
+                this.melding = $"Kan het bestand {this.source} niet inlezen.\nFoutmelding {e.Message}.";
+            }
+            return result;
+        }
+
     }
 }
