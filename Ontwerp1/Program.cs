df79a52d4fdc14b9f2c377f6e4d274af130e8545
codenameClass <jens.ingels-business@outlook.com>
2020-03-04 10:57:47 +0100

    Add project files.

---
+++ Ontwerp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ontwerp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
