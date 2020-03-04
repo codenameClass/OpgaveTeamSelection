3edb5b8f29922216fe119bebd7fb5abdd86655b1
Sieglinde Coppens <sieglinde.coppens@student.hogent.be>
2020-03-04 11:45:08 +0100

    Eerste test, team aangevuld

--- OpgaveTeamSelection/Team.cs
+++ OpgaveTeamSelection/Team.cs
@@ -6,5 +6,29 @@ namespace OpgaveTeamSelection
 {
     class Team
     {
+        public List<Speler> Spelers { get; set; }
+        public string TeamNaam { get; set; }
+
+        public Selectie SelectieAanmaken(int aantalDefenders, int aantalMidfielders, int aantalForwarders, Strategieën strategie)
+        {
+            
+
+        }
+        public void VoegSpelerToe(Speler speler)
+        {
+            //Moeten we hier testen of de speler er al in zit? 
+            //Moeten we zorgen dat de speler dan uit zijn eigen team verwijderd wordt? 
+
+            Spelers.Add(speler);
+            speler.Team = this;
+        }
+        public void VerwijderSpeler(Speler speler)
+        {
+            //Testen of de speler wel echt in het team zit. 
+
+            Spelers.Remove(speler);
+            speler.Team = null;
+        }
+
     }
 }
