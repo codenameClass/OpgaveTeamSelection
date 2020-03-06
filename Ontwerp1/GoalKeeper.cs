1cc1dfe75b26af030e2f3929dd7d525b7bd1f2ec
codenameClass <jens.ingels-business@outlook.com>
2020-03-06 12:21:23 +0100

    analyse

---
+++ Ontwerp1/GoalKeeper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+namespace OpgaveTeamSelection
+{
+    public class GoalKeeper : Speler
+    {
+        public GoalKeeper(string naam, int rugNummer, int rating, int caps, IEnumerable<GoalKeeperPosities> posities) : base(naam, rugNummer, rating, caps)
+        {
+            MogenlijkePosities = posities.ToList();
+        }
+        List<GoalKeeperPosities> MogenlijkePosities { get; set; }
+    }
+}
---
+++ Ontwerp1/Selectie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace OpgaveTeamSelection
+{
+    public class SelectieJI
+    {
+        //List<Defender> defenders, List<MidFielder> midfielders,List<Forward> forwards, List<GoalKeeper> goalKeepers
+
+        public Selectie(List<Defender> defenders, List<MidFielder> midfielders, List<Forward> forwards, List<GoalKeeper> goalKeepers, Speler aanvoerder)
+        {
+            if ((defenders.Count + midfielders.Count + forwards.Count + goalKeepers.Count) != 11)
+                throw new ArgumentException("Het SpelerAantal is niet gelijk aan 11");
+            Defenders = defenders;
+            MidFielders = midfielders;
+            Forwards = forwards;
+            GoalKeeper = goalKeepers;
+            Aanvoerder = aanvoerder;
+        }
+        public Speler Aanvoerder { get; set; }
+        public List<Defender> Defenders { get; set; }
+        public List<MidFielder> MidFielders { get; set; }
+        public List<Forward> Forwards { get; set; }
+        public List<GoalKeeper> GoalKeeper { get; set; }
+    }
+}
