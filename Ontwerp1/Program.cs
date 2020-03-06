ac5afc323ed036179d3f026120b89995b2c5879f
codenameClass <jens.ingels-business@outlook.com>
2020-03-06 09:10:18 +0100

    Test Exception

--- Ontwerp1/Program.cs
+++ Ontwerp1/Program.cs
@@ -7,6 +7,29 @@ namespace Ontwerp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            throw new PlayerinfoException(new NoIntFormatException("test"));
         }
+    }
+    class PlayerinfoException : Exception
+    {
+        public IPlayerinfoException Exception { get; set; }
+
+        public PlayerinfoException(IPlayerinfoException exception) => Exception = exception;
+    }
+    interface IPlayerinfoException
+    {
+
+    }
+    class NoIntFormatException : Exception, IPlayerinfoException
+    {
+        //FormatException
+        public NoIntFormatException(string message) : base(message)
+        {
+            
+        }
+    }
+    class NumberValueException : Exception, IPlayerinfoException
+    {
+
     }
 }
